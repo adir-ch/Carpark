059c02db269e09a5b5e93ebfe1d12bb24f614e0f
adir-ch <actiger@gmail.com>
2017-06-19 01:57:07 +1000

    Parking engine + parking engine integration test

---
+++ Carpark/Carpark.Engine.Test/Engine/ParkingEngineShould.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carpark.Engine.Test.Engine
+{
+    [TestFixture]
+    public class ParkingEngineShould
+    {
+        private ParkingEngine _engine; 
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _engine = new ParkingEngine(); 
+        }
+
+        [Test]
+        public void CalculateTotalParkingCostOnValidNightRateEntries()
+        {
+            DateTime enter = new DateTime(2017, 01, 01, 18, 0, 0);
+            DateTime exit = new DateTime(2017, 01, 02, 5, 59, 59);
+            var transaction = _engine.CalculateParkingRate(new Patron() { EnterTime = enter, ExitTime = exit });
+            Assert.That(transaction.TransactionTotal(), Is.EqualTo((decimal)6.5));  
+        }
+    }
+}
---
+++ Carpark/Carpark.Engine/ParkingEngine.cs
@@ -0,0 +1,32 @@
+﻿using Carpark.Engine.DecisionTree;
+using Carpark.Engine.ParkingRateRepository;
+using Carpark.Engine.RateRepository.Rates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carpark.Engine
+{
+    public class ParkingEngine
+    {
+        private ParkingRatesRepository _repository;
+        private DecisionQueryNode _parkingRateDecisionTree; 
+
+        public ParkingEngine()
+        {
+            _repository = new ParkingRatesRepository();
+            _parkingRateDecisionTree = ParkingRateDecisionTree.GetParkingRateDecisionTree();  
+        }
+
+        public ParkingTransaction CalculateParkingRate(Patron patron)
+        {
+            ParkingRateCode parkingRateCode = _parkingRateDecisionTree.Evaluate(patron);
+            var rate = _repository.GetParkingRate(parkingRateCode);
+
+            return new ParkingTransaction(patron, rate); 
+        }
+
+    }
+}
