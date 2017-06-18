fa064073894d7a0f91451b7cae212a32f9a3ff48
adir-ch <actiger@gmail.com>
2017-06-19 01:27:13 +1000

    Simple parking rate repo and rates creation factory

---
+++ Carpark/Carpark.Engine/ParkingRateRepository/ParkingRatesFactory.cs
@@ -0,0 +1,122 @@
+﻿using Carpark.Engine.ParkingRateRepository.Data;
+using Carpark.Engine.RateRepository;
+using Carpark.Engine.RateRepository.Calculators;
+using Carpark.Engine.RateRepository.Rates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carpark.Engine.ParkingRateRepository
+{
+    public class ParkingRatesFactory
+    {
+        // implementing some kind of caching since Parking rate does not need to be 
+        // created every time (for a specific paking rate)
+        private IDictionary<ParkingRateCode, IParkingRate> _parkingRatesMap;
+        private DbContext _db = DbContext.Instance; 
+
+        public ParkingRatesFactory()
+        {
+            _parkingRatesMap = new Dictionary<ParkingRateCode, IParkingRate>();
+        }
+
+        // created the function as virtual to have an extension point without overriding the 
+        // entire class implementation, only the location creation method. 
+        public virtual IParkingRate CreateParkingRate(ParkingRateCode parkingRateCode)
+        {
+            IParkingRate parkingRate = RetriveExistingParkingRate(parkingRateCode);
+            if (parkingRate != null)
+            {
+                return parkingRate; 
+            }
+
+            parkingRate = GenerateParkingRateObject(parkingRateCode);
+            AddCreatedParkingRate(parkingRateCode, parkingRate); 
+            return parkingRate; 
+        }
+
+        private IParkingRate RetriveExistingParkingRate(ParkingRateCode parkingRateCode)
+        {
+            IParkingRate parkingRate; 
+            _parkingRatesMap.TryGetValue(parkingRateCode, out parkingRate);
+            return parkingRate; 
+        }
+
+        private void AddCreatedParkingRate(ParkingRateCode parkingRateCode, IParkingRate parkingRateObject)
+        {
+            _parkingRatesMap.Add(parkingRateCode, parkingRateObject); // add for next time
+        }
+
+        protected virtual IParkingRate GenerateParkingRateObject(ParkingRateCode parkingRateCode)
+        {
+            IParkingRate parkingRate = null; 
+            switch (parkingRateCode)
+            {
+                case ParkingRateCode.EarlyBird:
+                    {
+                        parkingRate = CreateEarlyBirdRateObject(); 
+                        break;
+                    }
+                case ParkingRateCode.NightRate:
+                    {
+                        parkingRate = CreateNightRateObject();
+
+                        break;
+                    }
+                case ParkingRateCode.WeekendRate:
+                    {
+                        parkingRate = CreateWeekendRateObject();
+                        break;
+                    }
+                case ParkingRateCode.StandardHourRate:
+                case ParkingRateCode.StandardCalendarDayRate:
+                    {
+                        parkingRate = CreateStandardRateObject(); 
+                        break;
+                    }
+                default:
+                    {
+                        parkingRate = CreateStandardRateObject(); 
+                        break;
+                    }
+            }
+
+            return parkingRate; 
+        }
+
+        private IParkingRate CreateEarlyBirdRateObject()
+        {
+            return new EarlyBirdRate(new FlatRateCalculator()
+            {
+                Rate = _db.Read((int)ParkingRateCode.EarlyBird)
+            });
+        }
+
+        private IParkingRate CreateNightRateObject()
+        {
+            return new NightRate(new FlatRateCalculator()
+            {
+                Rate = _db.Read((int)ParkingRateCode.NightRate)
+            });
+        }
+
+        private IParkingRate CreateWeekendRateObject()
+        {
+            return new WeekendRate(new FlatRateCalculator()
+            {
+                Rate = _db.Read((int)ParkingRateCode.WeekendRate)
+            });
+        }
+
+        private IParkingRate CreateStandardRateObject()
+        {
+            return new StandardRate(new HourlyRateCalculator()
+            {
+                HourRate = _db.Read((int)ParkingRateCode.StandardHourRate),
+                CalendarDayRate = _db.Read((int)ParkingRateCode.StandardCalendarDayRate)
+            });
+        }
+    }
+}
---
+++ Carpark/Carpark.Engine/ParkingRateRepository/ParkingRatesRepository.cs
@@ -0,0 +1,32 @@
+﻿using Carpark.Engine.RateRepository.Rates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carpark.Engine.ParkingRateRepository
+{
+    public class ParkingRatesRepository
+    {
+        private ParkingRatesFactory _parkingRatesFactory;
+
+        public ParkingRatesRepository()
+        {
+            _parkingRatesFactory = new ParkingRatesFactory(); 
+        }
+
+        public IParkingRate GetParkingRate(ParkingRateCode parkingRateCode)
+        {
+            var parkingRate = _parkingRatesFactory.CreateParkingRate(parkingRateCode); 
+
+            if(parkingRate == null)
+            {
+                throw new Exception(String.Format("Unable to create a new parking rate with code: {0}", parkingRateCode)); 
+            }
+
+            return parkingRate; 
+        }
+
+    }
+}
