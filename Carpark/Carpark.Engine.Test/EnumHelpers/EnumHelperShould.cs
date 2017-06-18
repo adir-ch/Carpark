ea5681d7f8cb027c6a1400d7f453f1c817b71d6c
adir-ch <actiger@gmail.com>
2017-06-19 01:50:29 +1000

    Added a test class for the enum helper

---
+++ Carpark/Carpark.Engine.Test/EnumHelpers/EnumHelperShould.cs
@@ -0,0 +1,24 @@
+﻿using Carpark.Engine.RateRepository.Rates;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Carpark.Engine.ParkingRateRepository.Helpers; 
+
+namespace Carpark.Engine.Test
+{
+    [TestFixture]
+    public class EnumHelperShould
+    {
+        [Test]
+        [TestCase(ParkingRateCode.EarlyBird, ExpectedResult = "Early bird rate")]
+        [TestCase(ParkingRateCode.StandardHourRate, ExpectedResult = "Standard rate")]
+        [TestCase(ParkingRateCode.StandardCalendarDayRate, ExpectedResult = "Standard rate")]
+        public string RetriveDisplayAttribute(ParkingRateCode code)
+        {
+            return typeof(ParkingRateCode).GetEnumDescription((int?)code); 
+        }
+    }
+}
