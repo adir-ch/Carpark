6a58be064491db3513446408843ce0b1320df829
adir-ch <actiger@gmail.com>
2017-06-18 23:46:03 +1000

    Decision tree DateTime helper + tests

---
+++ Carpark/Carpark.Engine.Test/DecisionTree/Helpers/DateTimeHelperShould.cs
@@ -0,0 +1,75 @@
+﻿using Carpark.Engine.DecisionTree.Helpers;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carpark.Engine.Test.DecisionTree.Helpers
+{
+    [TestFixture]
+    public class DateTimeHelperShould
+    {
+        [Test, TestCaseSource(typeof(IsNightTestCases), "TestCases")]
+        public bool CheckIfQualifyForNightRate(DateTime enter, DateTime exit)
+        {
+            return DateTimeHelper.IsNight(enter, exit); 
+        }
+
+        [Test, TestCaseSource(typeof(IsEarlyBirdTestCases), "TestCases")]
+        public bool CheckIfQualifyForEarlyBirdRate(DateTime enter, DateTime exit)
+        {
+            return DateTimeHelper.IsEarlyBird(enter, exit);
+        }
+
+        [Test, TestCaseSource(typeof(IsWeekendTestCases), "TestCases")]
+        public bool CheckIfQualifyForWeekendRate(DateTime enter, DateTime exit)
+        {
+            return DateTimeHelper.IsWeekend(enter, exit);
+        }
+    }
+
+    public class IsNightTestCases
+    {
+        public static IEnumerable TestCases
+        {
+            get 
+            {
+                yield return new TestCaseData(new DateTime(2017, 01, 01, 18, 0, 0), new DateTime(2017, 01, 02, 6, 00, 00)).Returns(false);
+                yield return new TestCaseData(new DateTime(2017, 01, 01, 18, 0, 0), new DateTime(2017, 01, 02, 5, 59, 59)).Returns(true);
+                yield return new TestCaseData(new DateTime(2017, 01, 01, 17, 59, 59), new DateTime(2017, 01, 02, 5, 00, 00)).Returns(false); 
+            }
+        }
+    }
+
+    public class IsEarlyBirdTestCases
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new DateTime(2017, 01, 01, 7, 0, 0), new DateTime(2017, 01, 02, 16, 00, 00)).Returns(false);
+                yield return new TestCaseData(new DateTime(2017, 01, 01, 6, 0, 0), new DateTime(2017, 01, 01, 23, 30, 01)).Returns(false);
+                yield return new TestCaseData(new DateTime(2017, 01, 01, 5, 59, 59), new DateTime(2017, 01, 01, 15, 00, 01)).Returns(false);
+                yield return new TestCaseData(new DateTime(2017, 01, 01, 06, 01, 00), new DateTime(2017, 01, 01, 16, 00, 00)).Returns(true);
+            }
+        }
+    }
+
+    public class IsWeekendTestCases
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new DateTime(2017, 01, 07, 0, 0, 1), new DateTime(2017, 01, 17, 16, 00, 00)).Returns(false);
+                yield return new TestCaseData(new DateTime(2017, 01, 07, 6, 0, 0), new DateTime(2017, 01, 09, 00, 00, 00)).Returns(false);
+                yield return new TestCaseData(new DateTime(2017, 01, 06, 23, 59, 59), new DateTime(2017, 01, 08, 15, 00, 01)).Returns(false);
+                yield return new TestCaseData(new DateTime(2017, 01, 07, 06, 01, 00), new DateTime(2017, 01, 08, 23, 59, 59)).Returns(true);
+                yield return new TestCaseData(new DateTime(2017, 01, 07, 00, 00, 00), new DateTime(2017, 01, 08, 23, 59, 59)).Returns(true);
+            }
+        }
+    }
+}
---
+++ Carpark/Carpark.Engine/DecisionTree/Helpers/DateTimeHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carpark.Engine.DecisionTree.Helpers
+{
+    public static class DateTimeHelper
+    {
+        public static bool IsNight(DateTime enter, DateTime exit)
+        {
+            var span = (enter - exit);
+            Console.WriteLine("start: {0}, end: {1} => Days={2}, TotalDays={3}, hours: {4}", enter, exit, span.Days, span.TotalDays, span.Hours);
+
+            if (span.Days == 0 && span.Hours < 12)
+            {
+                if (enter.Hour >= 18 && exit.Hour < 6)
+                {
+                    Console.WriteLine("Found night rate");
+                    return true;
+                }
+            }
+
+            Console.WriteLine("Not a night rate");
+            return false; 
+        }
+
+        public static bool IsEarlyBird(DateTime enter, DateTime exit)
+        {
+            var sameDay = (exit.Date == enter.Date);
+            DateTime startExit = new DateTime(exit.Year, exit.Month, exit.Day, 15, 30, 00);
+            DateTime endExit = new DateTime(exit.Year, exit.Month, exit.Day, 23, 30, 00);
+            Console.WriteLine("start: {0}, end: {1} => same day: {2}", enter, exit, sameDay ? "Yes" : "No");
+
+            if (sameDay == true)
+            {
+                if (enter.Hour >= 6 && enter.Hour < 9)
+                {
+                    if (exit >= startExit && exit < endExit)
+                    {
+                        Console.WriteLine("Found Early bird rate");
+                        return true;
+                    }
+
+                }
+            }
+
+            Console.WriteLine("Not an early bird rate");
+            return false; 
+        }
+
+        public static bool IsWeekend(DateTime enter, DateTime exit)
+        {
+            var span = (exit - enter);
+            Console.WriteLine("start: {0}, end: {1} => Days={2}, TotalDays={3}, hours: {4}", enter, exit, span.Days, span.TotalDays, span.Hours);
+
+
+            if (span.Days < 2)
+            {
+                if (enter.DayOfWeek == DayOfWeek.Saturday || enter.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    if (exit.DayOfWeek == DayOfWeek.Saturday || exit.DayOfWeek == DayOfWeek.Sunday)
+                    {
+                        Console.WriteLine("Found weekend rate");
+                        return true;
+                    }
+                }
+            }
+
+            Console.WriteLine("Not a weekend rate");
+            return false; 
+        }
+    }
+}
