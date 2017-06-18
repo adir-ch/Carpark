22425d545bf39cfe2d26fca97c231f5e99eeb080
adir-ch <actiger@gmail.com>
2017-06-19 00:23:21 +1000

    Rate Calculators for flat and hourly

---
+++ Carpark/Carpark.Engine/ParkingRateRepository/Calculators/FlatRateCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carpark.Engine.RateRepository.Calculators
+{
+    public class FlatRateCalculator : IParkingRateCalaulator
+    {
+        public decimal Rate { get; set; }
+
+        public decimal CalculateRate(DateTime entryTime, DateTime exitTime)
+        {
+            return Rate; 
+        }
+    }
+}
---
+++ Carpark/Carpark.Engine/ParkingRateRepository/Calculators/HourlyRateCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carpark.Engine.RateRepository.Calculators
+{
+    public class HourlyRateCalculator : IParkingRateCalaulator
+    {
+        public decimal HourRate { get; set; }
+        public decimal CalendarDayRate { get; set; }
+
+        public decimal CalculateRate(DateTime entryTime, DateTime exitTime)
+        {
+            var timespan = (exitTime - entryTime); 
+
+            if (timespan.Days > 1)
+            {
+                return (timespan.Days * CalendarDayRate); 
+            }
+
+            return (timespan.Hours * HourRate); 
+        }
+    }
+}
