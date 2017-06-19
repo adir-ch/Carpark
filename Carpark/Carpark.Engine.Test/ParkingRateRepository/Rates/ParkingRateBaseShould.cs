0ffbdb45fb867cf663efce4c95b545f200afaf7c
adir-ch <actiger@gmail.com>
2017-06-19 13:01:26 +1000

    Check in the tests for the ParkingRateBase class

---
+++ Carpark/Carpark.Engine.Test/ParkingRateRepository/Rates/ParkingRateBaseShould.cs
@@ -0,0 +1,41 @@
+﻿using Carpark.Engine.RateRepository.Calculators;
+using Carpark.Engine.RateRepository.Rates;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carpark.Engine.Test.ParkingRateRepository.Rates
+{
+    [TestFixture]
+    public class ParkingRateBaseShould
+    {
+        private NightRate _rate;
+        private Mock<IParkingRateCalaulator> _calculator;
+        private DateTime _enterTime;
+        private DateTime _exitTime; 
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _calculator = new Mock<IParkingRateCalaulator>(); 
+            _rate = new NightRate(_calculator.Object);
+
+            _enterTime = new DateTime(2017, 01, 01, 18, 0, 0);
+            _exitTime = new DateTime(2017, 01, 02, 6, 00, 00);
+
+            _calculator.Setup(calc => calc.CalculateRate(It.IsAny<DateTime>(), It.IsAny<DateTime>())).Returns(100); 
+        }
+
+        [Test]
+        public void ReturnTotalParkingRateAmount()
+        {
+            var result = _rate.CalculateRate(_enterTime, _exitTime);
+            Assert.That(result, Is.EqualTo(100)); 
+            _calculator.Verify(f => f.CalculateRate(It.IsAny<DateTime>(), It.IsAny<DateTime>()), Times.Exactly(1)); 
+        }
+    }
+}
