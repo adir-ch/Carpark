2f789af2c2dfc077da3479a1e25c6b6fb2453802
adir-ch <actiger@gmail.com>
2017-06-19 13:52:43 +1000

    Repo tests are failing - refactoring

--- Carpark/Carpark.Engine.Test/ParkingRateRepository/ParkingRatesRpositoryShould.cs
+++ Carpark/Carpark.Engine.Test/ParkingRateRepository/ParkingRatesRpositoryShould.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using Carpark.Engine.ParkingRateRepository;
+using Carpark.Engine.RateRepository.Rates;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +12,31 @@ namespace Carpark.Engine.Test.ParkingRateRepository
     [TestFixture]
     public class ParkingRatesRpositoryShould
     {
-        [Test]
-        public void TestMethod()
+        private ParkingRatesRepository _repo; 
+
+        [OneTimeSetUp]
+        public void Init()
         {
-            // TODO: Add your test code here
-            Assert.Pass("Your first passing test");
+            _repo = new ParkingRatesRepository(); 
         }
+
+        [Test]
+        [TestCase(ParkingRateCode.EarlyBird, ExpectedResult=typeof(EarlyBirdRate))]
+        [TestCase(ParkingRateCode.WeekendRate, ExpectedResult = typeof(WeekendRate))]
+        [TestCase(ParkingRateCode.NightRate, ExpectedResult = typeof(NightRate))]
+        [TestCase(ParkingRateCode.StandardHourRate, ExpectedResult = typeof(StandardRate))]
+        [TestCase(ParkingRateCode.StandardCalendarDayRate, ExpectedResult = typeof(StandardRate))]
+        public Type ReturnCorrectParkingObjectByCode(ParkingRateCode code)
+        {
+            var rate = _repo.GetParkingRate(code);
+            return rate.GetType();  
+        }
+
+        [Test]
+        public void ThrowExceptionOnIllegalRateCode([Values(100, 200, 300)] int code)
+        {
+            Assert.Throws<Exception>(() => { _repo.GetParkingRate((ParkingRateCode)code); });
+        }
+
     }
 }
